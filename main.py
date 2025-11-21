b812dbdab2c86df452401e01c255c0c2dadd7d67
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-11-21 16:16:07 -0600

    created screen

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+
+pygame.init()
+
+# SCREEN
+screen = pygame.display.set_mode((500, 250))
+pygame.display.set_caption("Speed Typing Game")
+
+# GAME LOOP
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill("white")
+
+    # RENDER CODE HERE
+
+    pygame.display.flip()
+
+pygame.quit()
