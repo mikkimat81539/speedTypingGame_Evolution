b8cf16957713468e414f24e2c667e26ed043dc78
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-11-21 16:24:25 -0600

    created font class

---
+++ fontSetup.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+class setupFont:
+    def __init__(self, text, size, x_pos, y_pos):
+        self.text = text
+        self.size = size
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+    
+    def displayFont(self, screen, textbox):
+        createFont = pygame.font.Font("m5x7.ttf", self.size)
+        renderFont = createFont.render(self.text, False, "black")
+        screen.blit(renderFont, (self.x_pos, self.y_pos))
