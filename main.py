0e595ae1cde58641dfca13b4012b8252fac84718
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-11-21 16:54:39 -0600

    created button for mainMenu ENTER key

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ screen = pygame.display.set_mode((500, 250))
 pygame.display.set_caption("Speed Typing Game")
 
 # TextBox
-textbox = TextBox(209, 117, 70, 33, "black", 2, "ENTER")
+textbox = TextBox(209, 117, 70, 33, "black", 2, None)
 
 # Fonts
 headerFont = setupFont("Speed Typing Game", 50, 100, 70)
 
-paragraphFont = setupFont(f"Press {textbox.text} to begin", 30, 155, 120)
+paragraphFont = setupFont(f"Press ENTER to begin", 30, 155, 120)
 
 def mainMenu(screen):
 # GAME LOOP
@@ -24,7 +24,17 @@ def mainMenu(screen):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    textbox.color = "gray"
+                    textbox.border = 0
             
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RETURN:
+                    textbox.color = "black"
+                    textbox.border = 2
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse = pygame.mouse.get_pos()
                 print(mouse)
@@ -32,9 +42,9 @@ def mainMenu(screen):
         screen.fill("white")
 
         # RENDER CODE HERE
+        textbox.drawBox(screen)
         headerFont.displayFont(screen)
         paragraphFont.displayFont(screen)
-        textbox.drawBox(screen)
 
         pygame.display.flip()
 
